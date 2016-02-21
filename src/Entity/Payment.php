c4129cc4b2c367cf9ef719992074e2629174f848
rojoangel <rojo.angel@gmail.com>
2016-02-21 23:41:24 +0100

    add create method

--- src/Entity/Payment.php
+++ src/Entity/Payment.php
@@ -3,6 +3,7 @@
 namespace Entity;
 
 use Broadway\EventSourcing\EventSourcedAggregateRoot;
+use Event\Payment;
 
 class Payment extends EventSourcedAggregateRoot
 {
@@ -15,5 +16,15 @@ class Payment extends EventSourcedAggregateRoot
     public function getAggregateRootId()
     {
         return $this->paymentId;
+    }
+
+    /**
+     * @param string $paymentId
+     * @return \Entity\Payment
+     */
+    public static function create($paymentId) {
+        $payment = new Payment();
+        $payment->apply(new Payment\CreatedEvent($paymentId));
+        return $payment;
     }
 }
